2762da29dfc458207187d43461a7ea3cb1d128bb
Amanrathi07 <amanrathi4321@gmail.com>
2025-03-24 07:22:49 +0530

    auto geo add

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 let btn = document.querySelector("#searchBtn");
 
+let currLoc= document.getElementById("currLoc");
+
 btn.addEventListener("click", () => {
   console.log("Button clicked");
   let input = document.querySelector("#search");
 
   apiCall(input.value);
-});
+  input.value = "";
+  });
 
-async function apiCall(input) {
-  if (input) {
+async function apiCall(input,latitude,longitude) {
+ try{
+    
+  if (latitude && longitude){
+    let response = await fetch (`http://api.weatherapi.com/v1/current.json?key=49f88cffb66d403cad2211459252203&q=${latitude},${longitude}&aqi=no
+`);
+let data = await response.json();
+   
+    changeDisplay(data);
+    displayDiv(input);
+
+  }else{
     let response = await fetch(
       `http://api.weatherapi.com/v1/current.json?key=49f88cffb66d403cad2211459252203&q=${input}&aqi=no`
     );
-
     let data = await response.json();
-
+   
     changeDisplay(data);
-  } else {
-    alert("Please enter a city name");
+    displayDiv(input);
+
+  }
+    let data = await response.json();
+   
+    changeDisplay(data);
+    displayDiv(input);
+    
+  }catch(err){
+    console.log("Error");
   }
 }
 
@@ -28,9 +48,33 @@ function changeDisplay({ current, location }) {
   let humidity = document.getElementById("humidity");
   let pressure = document.getElementById("pressure");
 
+ 
+  
   temp.innerHTML = `${current.temp_c}<sup>o</sup>C`;
   city.innerHTML = `${location.name}`;
   windSpeed.innerHTML = `${current.wind_kph} km/h`;
   humidity.innerHTML = `${current.humidity}%`;
   pressure.innerHTML = `${current.pressure_mb} mb`;
 }
+
+
+function displayDiv(){
+  let weatherInfo = document.getElementById("weatherInfo");
+  let weatherMoreInfo = document.getElementById("weatherMoreInfo");
+
+  weatherInfo.style.display = "flex";
+  weatherMoreInfo.style.display = "flex";
+  console.log("displayed");
+  
+
+}
+
+
+currLoc.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition((position)=>{
+    let leti =position.coords.latitude;
+    let long = position.coords.longitude;
+    console.log(leti,long);
+    apiCall("",leti,long);
+  })
+});
